4839134477a279aa89fdb3bd4acf5d96606ca7b5
Stefan Ninic <ninicstefan94@gmail.com>
2018-06-23 15:51:20 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Http/Controllers/Front/Ajax/ClientScriptController.php
+++ src/Http/Controllers/Front/Ajax/ClientScriptController.php
@@ -3,49 +3,40 @@
 namespace KgBot\LaravelDeploy\Http\Controllers\Front\Ajax;
 
 use Illuminate\Http\Request;
-use KgBot\LaravelDeploy\Http\Controllers\BaseController;
 use KgBot\LaravelDeploy\Models\Client;
 use League\Flysystem\FileNotFoundException;
+use KgBot\LaravelDeploy\Http\Controllers\BaseController;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 
 class ClientScriptController extends BaseController
 {
-    public function fetch( Client $client )
+    public function fetch(Client $client)
     {
-        $filepath = base_path( DIRECTORY_SEPARATOR . $client->script_source );
+        $filepath = base_path(DIRECTORY_SEPARATOR.$client->script_source);
 
-        if ( file_exists( $filepath ) ) {
+        if (file_exists($filepath)) {
+            $content = file_get_contents($filepath);
 
-            $content = file_get_contents( $filepath );
-
-            return response()->json( compact( 'content' ) );
-
+            return response()->json(compact('content'));
         } else {
-
-            throw new FileNotFoundException( 'We can\'t find deploy script defined for this client' );
+            throw new FileNotFoundException('We can\'t find deploy script defined for this client');
         }
     }
 
-    public function save( Client $client, Request $request )
+    public function save(Client $client, Request $request)
     {
-        if ( $request->has( 'content' ) && $content = $request->get( 'content' ) ) {
+        if ($request->has('content') && $content = $request->get('content')) {
+            $filepath = base_path(DIRECTORY_SEPARATOR.$client->script_source);
 
-            $filepath = base_path( DIRECTORY_SEPARATOR . $client->script_source );
+            if (file_exists($filepath)) {
+                file_put_contents($filepath, $content);
 
-            if ( file_exists( $filepath ) ) {
-
-                file_put_contents( $filepath, $content );
-
-                return response()->json( 'success' );
-
+                return response()->json('success');
             } else {
-
-                throw new FileNotFoundException( 'We can\'t find deploy script defined for this client' );
+                throw new FileNotFoundException('We can\'t find deploy script defined for this client');
             }
-
         } else {
-
-            throw new BadRequestHttpException( 'Parameter content is required.' );
+            throw new BadRequestHttpException('Parameter content is required.');
         }
     }
 }
