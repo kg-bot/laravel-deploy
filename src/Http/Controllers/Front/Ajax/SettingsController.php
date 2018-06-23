37b66a20dcacd7f7c4f7760edaa3697d4f1222a9
Stefan Ninic <ninicstefan94@gmail.com>
2018-06-23 15:53:11 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- src/Http/Controllers/Front/Ajax/SettingsController.php
+++ src/Http/Controllers/Front/Ajax/SettingsController.php
@@ -2,78 +2,64 @@
 
 namespace KgBot\LaravelDeploy\Http\Controllers\Front\Ajax;
 
-use KgBot\LaravelDeploy\Http\Controllers\BaseController;
-use KgBot\LaravelDeploy\Jobs\DeployJob;
 use KgBot\LaravelDeploy\Models\Client;
+use KgBot\LaravelDeploy\Jobs\DeployJob;
 use KgBot\LaravelDeploy\Utils\LogParser;
+use KgBot\LaravelDeploy\Http\Controllers\BaseController;
 
 class SettingsController extends BaseController
 {
-
     /**
-     * Return last deploy log
+     * Return last deploy log.
      *
      * @return \Illuminate\Http\JsonResponse
      * @throws \Exception
      */
     public function lastLog()
     {
-
         $reader = $this->getLogs();
 
-        if ( is_null( $reader ) ) {
-
-            return response()->json( 'Log file path does not exist, check your configuration and try again.', 404 );
+        if (is_null($reader)) {
+            return response()->json('Log file path does not exist, check your configuration and try again.', 404);
         }
 
-        if ( count( $reader ) ) {
+        if (count($reader)) {
+            $log = $reader[0];
 
-            $log = $reader[ 0 ];
-
-            return response()->json( [ 'log' => $log ] );
-
+            return response()->json(['log' => $log]);
         } else {
-
-            return response()->json( 'Log is empty, deploy something!!!', 404 );
+            return response()->json('Log is empty, deploy something!!!', 404);
         }
     }
 
     /**
-     * Read deployment log file and return it's content
+     * Read deployment log file and return it's content.
      *
      * @return array|\KgBot\LaravelDeploy\Utils\LogParser|null
      * @throws \Exception
      */
     protected function getLogs()
     {
-        $log_file_name = config( 'laravel-deploy.log_file_name', 'laravel-log' );
-        $path          = storage_path( 'logs/' . $log_file_name );
+        $log_file_name = config('laravel-deploy.log_file_name', 'laravel-log');
+        $path = storage_path('logs/'.$log_file_name);
 
-        if ( file_exists( $path ) ) {
-
-            $file = file_get_contents( $path );
-            if ( $file ) {
-
+        if (file_exists($path)) {
+            $file = file_get_contents($path);
+            if ($file) {
                 $reader = new LogParser();
-                $reader = $reader->parse( $file );
+                $reader = $reader->parse($file);
 
                 return $reader;
-
             } else {
-
-                throw new \Exception( 'Couldn\'t read deployment log file.' );
+                throw new \Exception('Couldn\'t read deployment log file.');
             }
-
-
         } else {
-
-            return null;
-
+            return;
         }
     }
 
     /**
-     * Return collection of all deploy logs
+     * Return collection of all deploy logs.
      *
      * @return \Illuminate\Http\JsonResponse
      * @throws \Exception
@@ -82,39 +68,34 @@ class SettingsController extends BaseController
     {
         $reader = $this->getLogs();
 
-        if ( is_null( $reader ) ) {
-
-            return response()->json( 'Log file path does not exist, check your configuration and try again.', 404 );
+        if (is_null($reader)) {
+            return response()->json('Log file path does not exist, check your configuration and try again.', 404);
         }
 
-        if ( count( $reader ) ) {
-
-            return response()->json( [ 'logs' => collect( $reader ) ] );
-
+        if (count($reader)) {
+            return response()->json(['logs' => collect($reader)]);
         } else {
-
-            return response()->json( 'Log is empty, deploy something!!!', 404 );
+            return response()->json('Log is empty, deploy something!!!', 404);
         }
     }
 
     /**
-     * Start deployment from web dashboard
+     * Start deployment from web dashboard.
      *
      * @param \KgBot\LaravelDeploy\Models\Client $client
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function deployNow( Client $client )
+    public function deployNow(Client $client)
     {
+        dispatch(new DeployJob($client,
+            base_path($client->script_source)))->onQueue(config('laravel-deploy.queue'));
 
-        dispatch( new DeployJob( $client,
-            base_path( $client->script_source ) ) )->onQueue( config( 'laravel-deploy.queue' ) );
-
-        return response()->json( 'success' );
+        return response()->json('success');
     }
 
     /**
-     * Open settings page of web dashboard
+     * Open settings page of web dashboard.
      *
      * @return \Illuminate\Http\JsonResponse
      */
@@ -125,18 +106,17 @@ class SettingsController extends BaseController
          */
         $clients = Client::Active()->get();
 
-        $clients = $clients->each( function ( $client ) {
+        $clients = $clients->each(function ($client) {
+            $enabled = ($client->active) ? 'Enabled' : 'Disabled';
 
-            $enabled = ( $client->active ) ? 'Enabled' : 'Disabled';
-
-            return $client->text = $client->name . ' - ' . $enabled;
-        } );
+            return $client->text = $client->name.' - '.$enabled;
+        });
 
         $settings = [
 
-            'quick_deploy' => config( 'laravel-deploy.run_deploy' ),
+            'quick_deploy' => config('laravel-deploy.run_deploy'),
         ];
 
-        return response()->json( compact( 'clients', 'settings' ) );
+        return response()->json(compact('clients', 'settings'));
     }
 }
--- src/Jobs/DeployJob.php
+++ src/Jobs/DeployJob.php
@@ -2,26 +2,26 @@
 
 namespace KgBot\LaravelDeploy\Jobs;
 
+use Monolog\Logger;
 use Illuminate\Bus\Queueable;
+use Monolog\Handler\StreamHandler;
+use Monolog\Formatter\LineFormatter;
+use Illuminate\Queue\SerializesModels;
+use KgBot\LaravelDeploy\Models\Client;
+use Symfony\Component\Process\Process;
+use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
-use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 use KgBot\LaravelDeploy\Events\LaravelDeployFailed;
-use KgBot\LaravelDeploy\Events\LaravelDeployFinished;
 use KgBot\LaravelDeploy\Events\LaravelDeployStarted;
-use KgBot\LaravelDeploy\Models\Client;
-use Monolog\Formatter\LineFormatter;
-use Monolog\Handler\StreamHandler;
-use Monolog\Logger;
-use Symfony\Component\Process\Process;
+use KgBot\LaravelDeploy\Events\LaravelDeployFinished;
 
 class DeployJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     public $timeout = 240;
-    public $tries   = 1;
+    public $tries = 1;
 
     public $client;
     public $script_file;
@@ -37,19 +37,18 @@ class DeployJob implements ShouldQueue
     protected $process;
 
     /**
-     * @var string $command Command to be executed
+     * @var string Command to be executed
      */
     protected $command;
-
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct( Client $client, string $script_file )
+    public function __construct(Client $client, string $script_file)
     {
-        $this->client      = $client;
+        $this->client = $client;
         $this->script_file = $script_file;
         $this->setLogger();
         $this->setProcess();
@@ -57,30 +56,29 @@ class DeployJob implements ShouldQueue
 
     protected function setLogger()
     {
-        $this->logger = new Logger( 'laravel-deploy-logger' );
+        $this->logger = new Logger('laravel-deploy-logger');
 
-        $log_file_name = config( 'laravel-deploy.log_file_name', 'laravel-log' );
-        $stream        = new StreamHandler( storage_path( '/logs/' . $log_file_name ), Logger::DEBUG );
-        $formatter     = tap( new LineFormatter( null, null, true, true ), function ( $formatter ) {
+        $log_file_name = config('laravel-deploy.log_file_name', 'laravel-log');
+        $stream = new StreamHandler(storage_path('/logs/'.$log_file_name), Logger::DEBUG);
+        $formatter = tap(new LineFormatter(null, null, true, true), function ($formatter) {
             $formatter->includeStacktraces();
-        } );
-        $stream->setFormatter( $formatter );
+        });
+        $stream->setFormatter($formatter);
 
-
-        $this->logger->pushHandler( $stream );
+        $this->logger->pushHandler($stream);
     }
 
     protected function setProcess()
     {
-        $command = 'echo ' . config( 'laravel-deploy.user.password' );
-        $command .= ' | sudo -S -u ' . config( 'laravel-deploy.user.username' );
-        $command .= ' sh ' . $this->script_file;
+        $command = 'echo '.config('laravel-deploy.user.password');
+        $command .= ' | sudo -S -u '.config('laravel-deploy.user.username');
+        $command .= ' sh '.$this->script_file;
 
         $this->command = $command;
 
-        $process = new Process( $command );
-        $process->setTimeout( 500 );
-        $process->setIdleTimeout( 100 );
+        $process = new Process($command);
+        $process->setTimeout(500);
+        $process->setIdleTimeout(100);
 
         $this->process = $process;
     }
@@ -92,26 +90,22 @@ class DeployJob implements ShouldQueue
      */
     public function handle()
     {
-        event( new LaravelDeployStarted( $this->client, $this->command ) );
+        event(new LaravelDeployStarted($this->client, $this->command));
 
         $this->process->run();
 
-        if ( $this->process->isSuccessful() ) {
-
-            $this->logger->info( PHP_EOL . $this->process->getOutput(), [
-                'client' => json_encode( $this->client ),
+        if ($this->process->isSuccessful()) {
+            $this->logger->info(PHP_EOL.$this->process->getOutput(), [
+                'client' => json_encode($this->client),
                 'script' => $this->script_file,
-            ] );
-            event( new LaravelDeployFinished( $this->client, $this->process->getOutput() ) );
-
+            ]);
+            event(new LaravelDeployFinished($this->client, $this->process->getOutput()));
         } else {
-
-            $this->logger->critical( PHP_EOL . $this->process->getOutput(), [
-                'client' => json_encode( $this->client ),
+            $this->logger->critical(PHP_EOL.$this->process->getOutput(), [
+                'client' => json_encode($this->client),
                 'script' => $this->script_file,
-            ] );
-            event( new LaravelDeployFailed( $this->client, $this->process->getOutput() ) );
+            ]);
+            event(new LaravelDeployFailed($this->client, $this->process->getOutput()));
         }
-
     }
 }
